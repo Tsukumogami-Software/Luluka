5034101c1aa13ba95513f8cf9488164e97bb4dd0
Alice GG <hi@alicegg.tech>
2026-04-29 15:02:47 +0200

    No need for 3 packages

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-	"github.com/Tsukumogami-Software/Luluka/cmd"
+	"os"
+
+	"github.com/spf13/cobra"
 )
 
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "Luluka",
+	Short: "View and tweak shaders made with Kage for Ebiten",
+	Long: `Luluka helps you display Ebiten shaders in Kage.
+	Simply load a shader by indicating its path:
+		luluka shader.kage
+	`,
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		Run(args[0])
+	},
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
 func main() {
-	cmd.Execute()
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
--- shader.go
+++ shader.go
@@ -1,4 +1,4 @@
-package shader
+package main
 
 import (
 	"log"
